e23c85bf08dd60ff3c2e84d826b545dad4c9dca1
kargonpc <idprokopev@gmail.com>
2025-10-22 23:20:29 +0300

    done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
+use std::fs;
 
+fn main() {
+    let mut p = String::new();
+    std::io::stdin().read_line(&mut p).unwrap();
+    
+    if fs::read(p.trim()).is_ok() {
+        println!("success");
+    } else {
+        println!("failure");
+    }
 }
